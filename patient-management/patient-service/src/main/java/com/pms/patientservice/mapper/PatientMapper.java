120df75364cf54abd8ee7679be7a697770750d15
Chinmay-innovates <chinmaygope69@gmail.com>
2025-03-26 13:24:49 +0530

    create patient controller

--- patient-management/patient-service/src/main/java/com/pms/patientservice/mapper/PatientMapper.java
+++ patient-management/patient-service/src/main/java/com/pms/patientservice/mapper/PatientMapper.java
@@ -1,11 +1,15 @@
 package com.pms.patientservice.mapper;
 
+import com.pms.patientservice.dto.PatientRequestDTO;
 import com.pms.patientservice.dto.PatientResponseDTO;
 import com.pms.patientservice.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
         PatientResponseDTO patientDTO = new PatientResponseDTO();
+
         patientDTO.setId(patient.getId().toString());
         patientDTO.setName(patient.getName());
         patientDTO.setAddress(patient.getAddress());
@@ -13,5 +17,17 @@ public class PatientMapper {
         patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
 
         return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisterDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+
+        return patient;
     }
 }
