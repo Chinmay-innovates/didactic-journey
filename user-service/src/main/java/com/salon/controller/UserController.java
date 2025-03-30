8eac0a5752ed2ac4629e9b39dbcae69830b34303
Chinmay-innovates <chinmaygope69@gmail.com>
2025-03-30 16:08:19 +0530

    crud operations in spring boot

---
+++ user-service/src/main/java/com/salon/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.salon.controller;
+
+import com.salon.model.User;
+import com.salon.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/api/users")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/api/users")
+    public List<User> getUser() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/api/users/{userId}")
+    public User getUserById(@PathVariable("userId") Long id) throws Exception {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        }
+        throw new Exception("User not found");
+    }
+
+    @PutMapping("/api/users/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Long id) throws Exception {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isEmpty()) {
+            throw new Exception("User not found with id " + id);
+        }
+
+        User existingUser = userOptional.get();
+        existingUser.setFullName(user.getFullName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setRole(user.getRole());
+
+        return userRepository.save(existingUser);
+    }
+
+    @DeleteMapping("/api/users/{id}")
+    public String deleteUserById(@PathVariable Long id) throws Exception {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isEmpty()) {
+            throw new Exception("User doesn't exist with id " + id);
+        }
+        userRepository.deleteById(userOptional.get().getId());
+        return "User deleted with id " + id;
+    }
+}
